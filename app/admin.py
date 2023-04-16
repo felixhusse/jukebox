170b5a4355e042887558490980fc01c4a5251cf8
felix.husse <felix.husse@gmail.com>
2023-04-16 20:23:33 +0200

    - added MusicCard to admin.py

--- app/admin.py
+++ app/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Configuration
+from .models import Configuration, MusicCard
 # Register your models here.
 
 admin.site.register(Configuration)
+admin.site.register(MusicCard)
