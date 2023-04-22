3b9967dce7c74c381fd6d6fa4ef6b368007e9238
felix.husse <felix.husse@gmail.com>
2023-04-22 09:20:20 +0200

    - fixed none error

--- app/threads.py
+++ app/threads.py
@@ -22,6 +22,7 @@ class RFIDReaderThread(Thread):
         scope = "user-read-playback-state,user-modify-playback-state"
         spotify_player = SpotifyPlayer(spotiy_connection=SpotifyConnection(scope=scope))
         playing_song = False
+        none_count = 0
         try:
             if configuration:
                 print("Configuration found")
@@ -32,11 +33,17 @@ class RFIDReaderThread(Thread):
                         if musiccard:
                             spotify_player.play_song(musiccard.first().spotify_uid)
                             playing_song = True
+                            none_count = 0
                             print("Song started")
+                    elif uid and playing_song:
+                        none_count = 0
                     elif not uid and playing_song:
-                        spotify_player.stop_song()
-                        playing_song = False
-                        print("Song stopped")
+                        none_count = none_count + 1
+                        if none_count > 1:
+                            spotify_player.stop_song()
+                            playing_song = False
+                            none_count = 0
+                            print("Song stopped")
 
                     time.sleep(1.0)
         finally:
