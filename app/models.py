5cbe9bd725f95c55c577c3eb1de32f2b75e95d8e
felix.husse <felix.husse@gmail.com>
2023-05-05 20:52:33 +0200

    - added migration script

--- app/models.py
+++ app/models.py
@@ -7,7 +7,7 @@ class Configuration(models.Model):
     spotify_client_id = models.CharField(max_length=100)
     spotify_client_secret = models.CharField(max_length=100)
     spotify_callback_url = models.CharField(max_length=100)
-
+    spotify_speaker_id = models.CharField(max_length=100, default="")
 
 class MusicCard(models.Model):
     MUSIC_TYPE = [
