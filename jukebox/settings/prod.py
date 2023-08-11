b626a0e321fc18bfdc3339a1aaada42ef5a82934
felix.husse <felix.husse@gmail.com>
2023-08-11 10:50:09 +0200

    changed settings handling

---
+++ jukebox/settings/prod.py
@@ -0,0 +1,33 @@
+# prod.py
+# Production settings for myapp
+from . import *  # Import base settings from settings/__init__.py
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "root": {"level": env('LOG_LEVEL'), "handlers": ["file"]},
+    "handlers": {
+        "file": {
+            "level": env('LOG_LEVEL'),
+            "class": "logging.FileHandler",
+            "filename": "/var/log/jukebox.log",
+            "formatter": "app",
+        },
+    },
+    "loggers": {
+        "django": {
+            "handlers": ["file"],
+            "level": env('LOG_LEVEL'),
+            "propagate": True
+        },
+    },
+    "formatters": {
+        "app": {
+            "format": (
+                u"%(asctime)s [%(levelname)-8s] "
+                "(%(module)s.%(funcName)s) %(message)s"
+            ),
+            "datefmt": "%Y-%m-%d %H:%M:%S",
+        },
+    },
+}
